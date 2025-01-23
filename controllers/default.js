5085778f49aea399aa93f91041b88d5777b28574
Mlekce <stojanac.srdjan@gmail.com>
2025-01-23 07:51:44 +0100

    nodemailer support

--- controllers/default.js
+++ controllers/default.js
@@ -1,3 +1,4 @@
+const Customer = require("../data/database")
 function getHome(req, res) {
     res.send("Hello from express Server")
 }
@@ -6,11 +7,15 @@ function getSignup(req, res) {
     res.render("signupPage")
 }
 
-function postSignup(req, res){
+function postSignup(req, res) {
+    let data = req.body;
+    let newCustomer = new Customer(data.fname, data.lname, data.passwd, data.cpasswd, data.email, data.phone, data.country, data.city, data.state, data.pcode, data.address);
+    newCustomer.addCustomer()
     return
 }
 
 module.exports = {
     getHome,
-    getSignup
+    getSignup,
+    postSignup
 }
---
+++ utils/nodeMailer.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+const sender = {
+    user: "test@coded.in.rs",
+    pass: "secretpassword",
+    alias: "My awesome webshop"
+}
+
+const transporter = nodemailer.createTransport({
+  host: "box.coded.in.rs",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: sender.user,
+    pass: sender.pass,
+  },
+});
+
+async function sendMail(recipient) {
+  const info = await transporter.sendMail({
+    from: `${sender.alias} <${sender.user}>`,
+    to: recipient,
+    subject: "Registration",
+    text: `Hello, and welcome to my awesome webshop. Please click on this link to confirm registration: ${link}`, // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+sendMail('x@coded.in.rs').catch(console.error);
